d5ebd40eecc6b70649a21e445e68bbca16cc110c
MedinaKrelani <medina.krelani@studentet.uni-pr.edu>
2019-11-19 15:53:46 +0100

    Vendosja e projektit

---
+++ Steganography/Steganography/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Steganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(189, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 263);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOpenFile.Location = new System.Drawing.Point(189, 298);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(125, 37);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = false;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDecode.Location = new System.Drawing.Point(389, 514);
+            this.btnDecode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(189, 41);
+            this.btnDecode.TabIndex = 2;
+            this.btnDecode.Text = "Decode Here";
+            this.btnDecode.UseVisualStyleBackColor = false;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnEncode.Location = new System.Drawing.Point(189, 514);
+            this.btnEncode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(195, 41);
+            this.btnEncode.TabIndex = 3;
+            this.btnEncode.Text = "Encode Here";
+            this.btnEncode.UseVisualStyleBackColor = false;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Message :";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(357, 313);
+            this.textBoxFilePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(201, 22);
+            this.textBoxFilePath.TabIndex = 5;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(357, 391);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(201, 22);
+            this.textBoxMessage.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 663);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
+
---
+++ Steganography/Steganography/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Eshte butoni me ane te cilit ne mund te marrim foton ne te cilen duam te fshehim tekstin
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+           
+            
+                //Hapim dritaren ku do ta marrim File-in
+                OpenFileDialog hapjaEDialogut = new OpenFileDialog();
+
+                //Me kete kemi specifkuar tipin e file-it
+                hapjaEDialogut.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                //Specifikimi i path-it ku ndodhet fajlli me te cilin duam te manipulojme
+                hapjaEDialogut.InitialDirectory = @"C:\Users\Twin\Desktop";
+
+                //Vendosja e path-it te fajllit
+                if (hapjaEDialogut.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxFilePath.Text = hapjaEDialogut.FileName.ToString();
+                    pictureBox1.ImageLocation = textBoxFilePath.Text;
+                }
+            }
+
+         //Krijimi i butonit per Enkodim
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            //Krijimi i nje insance te klases Bitmap per te manipuluar me fotot qe jane te definuara ne pixels
+            Bitmap imazhi = new Bitmap(textBoxFilePath.Text);
+
+            //Me ane te unazave po pershkojme Width&Height te file-it
+            for (int i = 0; i < imazhi.Width; i++)
+            {
+                for (int j = 0; j < imazhi.Height; j++)
+                {
+                    //Imazhit te mapuar po i marrim secilin pixel
+                    Color pixel = imazhi.GetPixel(i, j);
+
+                    //Shfaqja e ngjyrave RGB per secilin pixel
+                    if (i < 1 && j < textBoxMessage.TextLength)
+                    {
+                        Console.WriteLine(" R= [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine(" G= [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine(" B= [" + i + "][" + j + "] = " + pixel.B);
+
+                        //Konvertimi me qellim te shfaqjes ne console te shkronjes perkatese
+                        char shkronja = Convert.ToChar(textBoxMessage.Text.Substring(j, 1));
+                        int vleraEKonvertuar = Convert.ToInt32(shkronja);
+                        Console.WriteLine("shkronja : " + shkronja + " vleraEKonvertuar : " + vleraEKonvertuar);
+
+                        imazhi.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, vleraEKonvertuar));
+
+                    }
+
+                    if (i == imazhi.Width - 1 && j == imazhi.Height - 1)
+                    {
+                        imazhi.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, textBoxMessage.TextLength));
+                    }
+                }
+            }
+
+            //Krijohet mundesia per ruajtjen e File-it
+            SaveFileDialog ruajtjaEFajllit = new SaveFileDialog();
+
+            ruajtjaEFajllit.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            ruajtjaEFajllit.InitialDirectory = @"C:\Users\Twin\Desktop";
+
+            if (ruajtjaEFajllit.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = ruajtjaEFajllit.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+                imazhi.Save(textBoxFilePath.Text);
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap imazhi = new Bitmap(textBoxFilePath.Text);
+            string tekstiFshehur = "";
+
+            //Ngjashem kalojme neper secilin pixel te imazhit
+            Color lastpixel = imazhi.GetPixel(imazhi.Width - 1, imazhi.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < imazhi.Width; i++)
+            {
+                for (int j = 0; j < imazhi.Height; j++)
+                {
+                    Color pixel = imazhi.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string shkronja = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+                        tekstiFshehur = tekstiFshehur + shkronja;
+                    }
+                }
+            }
+            textBoxMessage.Text = tekstiFshehur;
+        }
+    
+}
+    }
+    
+
